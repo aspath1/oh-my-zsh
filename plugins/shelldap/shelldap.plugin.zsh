4d95afd1151f4910a5432891c9c26c6b8d2b06e6
Remi Paulmier <remi.paulmier@gmail.com>
2014-03-22 11:35:34 +0100

    added plugin for shelldap

---
+++ plugins/shelldap/shelldap.plugin.zsh
@@ -0,0 +1,9 @@
+# Authors:
+# https://github.com/shtouff
+#
+# Shelldap-related zsh aliases and functions for zsh
+
+# Aliases ###################################################################
+alias ldap_data="shelldap --server comcorp1 --binddn cn=admin,dc=comuto,dc=com --basedn dc=comuto,dc=com"
+alias ldap_config="shelldap --server comcorp1 --binddn cn=admin,cn=config --basedn cn=config"
+
